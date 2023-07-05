171e3456d56e6fa1c69fed9167e2b53d045eb6e1
Breno Maia <39398165+bremaya@users.noreply.github.com>
2023-07-05 04:31:36 -0300

    Update fdmrepo.sh

--- fdmrepo.sh
+++ fdmrepo.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 #Created by Breno Maia (https://github.com/bremaya)
-wget -q -O- https://raw.githubusercontent.com/bremaya/Repos/main/fdm.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/freedownloadmanager.gpg
+wget -q -O- https://raw.githubusercontent.com/bremaya/Repos/main/fdm.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/freedownloadmanager.gpg &&
 echo "deb [arch=amd64] https://debrepo.freedownloadmanager.org/ bionic main" | sudo tee /etc/apt/sources.list.d/freedownloadmanager.list
