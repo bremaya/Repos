5eb2813ebc93b28b5f3a8035b16f89a187580298
Breno Maia <39398165+bremaya@users.noreply.github.com>
2023-07-05 04:32:05 -0300

    Update gcrepo.sh

--- gcrepo.sh
+++ gcrepo.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 #Created by Breno Maia (https://github.com/bremaya)
-echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list &&
 wget -q -O- https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/google-chrome.gpg
