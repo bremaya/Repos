fc58a9f2dd86ec5be4d81bf88a24b1d6a4b59045
Breno Maia <bremaya@gmail.com>
2023-07-10 05:47:40 -0300

    Added new packages

--- ppa/UpdatePackages.sh
+++ ppa/UpdatePackages.sh
@@ -1,8 +1,10 @@
 # /bin/bash
 # Source : https://assafmo.github.io/2019/05/02/ppa-repo-hosted-on-github.html
+
 # Setting variables
 read -p "Enter your PPA e-mail: " PPAEMAIL
-read -p "Commit description: " COMMITDESC
+read -p "Enter the commit description: " COMMITDESC
+
 # Packages & Packages.gz
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
@@ -10,9 +12,10 @@ gzip -k -f Packages
 # Release, Release.gpg & InRelease
 apt-ftparchive release . > Release
 gpg --default-key "$PPAEMAIL" -abs -o - Release > Release.gpg
-gpg --default-key "$COMMITDESC" --clearsign -o - Release > InRelease
+gpg --default-key "$PPAEMAIL" --clearsign -o - Release > InRelease
 
 # Commit & push
+cd ..
 git add -A
-git commit -m update
+git commit -m "$COMMITDESC"
 git push
