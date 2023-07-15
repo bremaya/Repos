587af1c7f4ebd0733fd2ba2cfc78fd707002e071
Breno Maia <39398165+bremaya@users.noreply.github.com>
2023-07-15 05:01:54 -0300

    Update merepo.sh
    Minor changes

--- merepo.sh
+++ merepo.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 #Created by Breno Maia (https://github.com/bremaya)
-wget -q -O- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft-edge.gpg >/dev/null &&
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft-edge.gpg >/dev/null &&
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge/ stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge.list >/dev/null
