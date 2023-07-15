2bff1a902672a2d3296d0a36e09489a0c412380d
Breno Maia <39398165+bremaya@users.noreply.github.com>
2023-07-14 23:33:43 -0300

    Create yxrepo.sh
    Created yxrepo.sh

---
+++ yxrepo.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+#Created by Breno Maia (https://github.com/bremaya)
+wget -q -O- https://github.com/bremaya/Repos/raw/main/yandex.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/yandex-browser.gpg >/dev/null &&
+echo "deb [arch=amd64] https://repo.yandex.ru/yandex-browser/deb stable main" | sudo tee /etc/apt/sources.list.d/yandex-browser.list >/dev/null
